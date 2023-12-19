3fb8f736a1e80c1967593b38697e2d92f04794f8
coco <tldjs3651@naver.com>
2023-12-19 17:34:31 +0900

    post tweet form update

---
+++ src/components/post-tweet-form.tsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import styled from "styled-components";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const TextArea = styled.textarea`
+  border: 2px solid white;
+  padding: 20px;
+  border-radius: 20px;
+  font-size: 16px;
+  color: white;
+  background-color: black;
+  width: 100%;
+  resize: none;
+  &::placeholder {
+    font-size: 16px;
+    font-family: system-ui, -apple-system, BlinkMacSystemFont, "Segoe UI", Robot,
+      Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  }
+  &:focus {
+    outline: none;
+    border-color: #1d9bf0;
+  }
+`;
+
+const AttachFileButton = styled.label`
+  padding: 10px 0px;
+  color: #1d9bf0;
+  text-align: center;
+  border-radius: 20px;
+  border: 2px solid #1d9bf0;
+  font-size: 14px;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const AttachFileInput = styled.input`
+  display: none;
+`;
+
+const SubmitBtn = styled.input`
+  background-color: #1d9bf0;
+  color: white;
+  border: none;
+  padding: 10px 0px;
+  border-radius: 20px;
+  font-size: 16px;
+  cursor: pointer;
+  &:hover,
+  &:active {
+    opacity: 0.9;
+  }
+`;
+
+export default function PostTweetForm() {
+  const [isLoading, setLoading] = useState(false);
+  const [tweet, setTweet] = useState("");
+  const [file, setFile] = useState<File | null>(null);
+  const onChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    setTweet(e.target.value);
+  };
+  const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    // e.target에 파일이 존재하는지 체크
+    // 파일이 존재한다면 하나만 있는지 체크 -> 2개 이상 올리지 않기 때문
+    const { files } = e.target;
+    if (files && files.length === 1) {
+      setFile(files[0]);
+    }
+  };
+  return (
+    <Form>
+      <TextArea
+        rows={5}
+        maxLength={180}
+        onChange={onChange}
+        value={tweet}
+        placeholder="what is happening?"
+      />
+      <AttachFileButton htmlFor="file">
+        {file ? "Photo added ✅" : "Add Photo"}
+      </AttachFileButton>
+      <AttachFileInput
+        onChange={onFileChange}
+        type="file"
+        id="file"
+        accept="image/*"
+      />
+      <SubmitBtn
+        type="submit"
+        value={isLoading ? "Posting..." : "Post Tweet"}
+      />
+    </Form>
+  );
+}
